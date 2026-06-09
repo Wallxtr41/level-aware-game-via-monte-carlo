14b18082c6354d1752f461fe10e4d21a7fae0d1a
Hüseyin Acemli <huseyin.acemli@ozu.edu.tr>
2026-06-09 21:06:06 +0300

    change the game design. instead of using different tile for different road component, it makes it same road tile for each road cell

--- main.py
+++ main.py
@@ -14,24 +14,11 @@ ITEM_PIXELS = ITEM_SIZE * SCALE
 
 BASE_DIR = Path(__file__).resolve().parent
 TILES_DIR = BASE_DIR / "tiles"
+ROAD_TILE_PATH = TILES_DIR / "default_road" / "road.png"
 
 TILE_PATHS = {
     "wall": TILES_DIR / "wall" / "wall.png",
-    "road_two_edge_up_down": TILES_DIR / "road_two_edge" / "road_two_edge_up_down.png",
-    "road_two_edge_right_left": TILES_DIR / "road_two_edge" / "road_two_edge_right_left.png",
-    "road_one_corner_northeast": TILES_DIR / "road_one_corner" / "road_one_corner_northeast.png",
-    "road_one_corner_southeast": TILES_DIR / "road_one_corner" / "road_one_corner_southeast.png",
-    "road_one_corner_southwest": TILES_DIR / "road_one_corner" / "road_one_corner_southwest.png",
-    "road_one_corner_northwest": TILES_DIR / "road_one_corner" / "road_one_corner_northwest.png",
-    "road_two_corner_up": TILES_DIR / "road_two_corner" / "road_two_corner_up.png",
-    "road_two_corner_right": TILES_DIR / "road_two_corner" / "road_two_corner_right.png",
-    "road_two_corner_down": TILES_DIR / "road_two_corner" / "road_two_corner_down.png",
-    "road_two_corner_left": TILES_DIR / "road_two_corner" / "road_two_corner_left.png",
-    "threeway_up": TILES_DIR / "threeway" / "threeway_up.png",
-    "threeway_right": TILES_DIR / "threeway" / "threeway_right.png",
-    "threeway_down": TILES_DIR / "threeway" / "threeway_down.png",
-    "threeway_left": TILES_DIR / "threeway" / "threeway_left.png",
-    "crossroads": TILES_DIR / "crossroads" / "crossroads.png",
+    "road": ROAD_TILE_PATH,
     "door_closed": TILES_DIR / "items" / "dumb_closed_door.png",
     "door_open": TILES_DIR / "items" / "dumb_open_door.png",
     "item_stamina": TILES_DIR / "items" / "dumb_stamina.png",
@@ -69,48 +56,7 @@ def is_road(map_data, row, col):
 
 
 def get_road_tile(tiles, map_data, row, col):
-    up = is_road(map_data, row - 1, col)
-    right = is_road(map_data, row, col + 1)
-    down = is_road(map_data, row + 1, col)
-    left = is_road(map_data, row, col - 1)
-
-    road_count = up + right + down + left
-
-    if road_count == 1:
-        if up:
-            return tiles["road_two_corner_down"]
-        if right:
-            return tiles["road_two_corner_left"]
-        if down:
-            return tiles["road_two_corner_up"]
-        return tiles["road_two_corner_right"]
-
-    if road_count == 2:
-        if up and down:
-            return tiles["road_two_edge_up_down"]
-        if left and right:
-            return tiles["road_two_edge_right_left"]
-        if up and right:
-            return tiles["road_one_corner_southwest"]
-        if right and down:
-            return tiles["road_one_corner_northwest"]
-        if down and left:
-            return tiles["road_one_corner_northeast"]
-        return tiles["road_one_corner_southeast"]
-
-    if road_count == 3:
-        if not up:
-            return tiles["threeway_up"]
-        if not right:
-            return tiles["threeway_right"]
-        if not down:
-            return tiles["threeway_down"]
-        return tiles["threeway_left"]
-
-    if road_count == 4:
-        return tiles["crossroads"]
-
-    return tiles["road_two_corner_up"]
+    return tiles["road"]
 
 
 def get_tile_surface(tiles, map_data, row, col, door_position):
